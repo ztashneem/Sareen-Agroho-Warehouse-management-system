56b7d072a6d42087874b4dd07f9ddd475dcdc503
ztashneem <z.tashneem12@gmail.com>
2022-05-09 21:46:20 +0600

    ITEM DELETE API

--- index.js
+++ index.js
@@ -66,6 +66,15 @@ async function run() {
             res.send(result);
         });
 
+        
+        //ITEM DELETE API
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 
